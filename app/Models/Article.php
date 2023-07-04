0388fd2fc006a9726c2ab24704f62e60af4d5784
HojiakbarSultonov <hojiakbarrasulovich@gmail.com>
2023-07-04 12:21:18 +0500

    searchable method

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use MoonShine\Fields\BelongsToMany;
+use MoonShine\Fields\HasMany;
+use MoonShine\Models\MoonshineUser;
+use MoonShine\Tests\Fixtures\Models\Category;
 
 class Article extends Model
 {
@@ -14,7 +19,22 @@ protected $casts = [
     'Files' => 'collection',
     'Data'=>'collection',
     'active'=>'bool'
-]   ;
+];
+public function categories(): BelongsToMany{
+    return $this->belongsToMany(Category::class);
+}
+
+    public function author(): BelongsTo{
+        return $this->BelongsTo(MoonshineUser::class);
+    }
+
+
+    public function comments(): HasMany{
+        return $this->BelongsTo(MoonshineUser::class);
+    }
+
+
+
 
 }
 
